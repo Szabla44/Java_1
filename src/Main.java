f03c24b5721bf2c6c9c5bff8c85ab05b09559313
Szabla44 <sogeking250@wp.pl>
2021-10-10 19:23:22 +0200

    fa

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ public class Main {
     public static void main(String[] args) {
 //        ćw.1 napisać po kilka przykładów wypisywania tekstu w konsoli,
 //        Użyć każdej z opcji
-
+        System.out.println("xdd");
+        System.out.print("aaaaa");
+        System.out.printf("xxxxxxxxxxxxxx");
 
     }
 }
