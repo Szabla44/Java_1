2fc92742b84c66a661f34996593ab18d52bfc6ee
Szabla44 <sogeking250@wp.pl>
2021-10-10 19:40:53 +0200

    zad3

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,25 @@ public class Main {
         /*  zad.2 zadeklarować 2 zmienne typu int i wykonać na nich obliczenia arytmetyczn (+, -, *, /, %)
         *   wynik wyświetlić w konsoli programu
         */
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Podaj imię: ");
+        String imie=scan.next();
+        System.out.print("Podaj nazwisko: ");
+        String nazwisko=scan.next();
+        System.out.print("Podaj nrIndeksu: ");
+        int nrindeksu=scan.nextInt();
 
+        System.out.println(imie+" " + nazwisko+" " +nrindeksu);
+        System.out.println();
+
+        System.out.print("Podaj a: ");
+        int a=scan.nextInt();
+        System.out.print("Podaj b: ");
+        int b=scan.nextInt();
+        System.out.println("suma: "+(a+b));
+        System.out.println("roznica: "+(a-b));
+        System.out.println("mnozenie: "+(a*b));
+        System.out.println("dzielenie: "+(a/b));
+        System.out.println("modulo: "+(a%b));
     }
 }
