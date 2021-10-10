1d0155c85b835e5a91c04611462aca94529f98f9
Szabla44 <sogeking250@wp.pl>
2021-10-10 19:05:25 +0200

    fafaf

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,42 @@ public class Main {
     public static void main(String[] args) {
 
         int zmiennaInt = 7;
-        System.out.println("zmienna int = " + zmiennaInt);
+        //ystem.out.println("zmienna int = " + zmiennaInt);
         /* ćw.2 */
 
         /*   zad.1 zadeklaruj i zainicjalizuj zmienne różnych typów i wyświetl w konsoli (im więcej tym lepiej ;) ) */
+
+        /*
 
         /*
         *    zad.2 Napisz kod który będzie wykonywał operacje: dodawania, odejmowania,
         *    mnożenia, dziealenia i modulo na zmiennych:
         *    a) liczbaA i liczbaB, dowolne liczby calkowite (int),
         *    b) liczbaX i liczbaY, dowolne liczby zmiennoprzecinkowe (double)
-        *  */
+        *
+         */
+
+        byte zmienna1=22;
+        short zmiann2=1231;
+        int mienna3=10211;
+        float zmiennaf=22.5f;
+        boolean zmiennab=true;
+
+        int a=6,b=3;
+        System.out.println("LICZBY CALKOWITE: ");
+        System.out.println("dodawanie "+ (a+b));
+        System.out.println("odejmowanie "+ (a-b));
+        System.out.println("mnożenie "+ (a*b));
+        System.out.println("dzielenie "+ (a/b));
+
+
+        double aa=8.5f,bb=4.8f;
+        System.out.println("LICZBY ZMIENNOPRZECINKOWE: ");
+        System.out.println("dodawanie "+ (aa+bb));
+        System.out.println("odejmowanie "+ (aa-bb));
+        System.out.println("mnożenie "+ (aa*bb));
+        System.out.println("dzielenie "+ (aa/bb));
+
 
 
 
